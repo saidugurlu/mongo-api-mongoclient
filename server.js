13b89c047fcc0d08db0daeb603cbef849f0adbf0
saidugurlu <ugurlumsaid@gmx.de>
2022-06-30 11:43:06 +0200

    connect to MongoDB finished and tested, work

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import { MongoClient } from 'mongodb'; // normalde mongoose daha iyi burada cok kücü bir proje oldugu icin bunu kullandik.
+
+
+//connect to MongoDB
+const conn = 'mongodb://localhost:27017';
+const client = new MongoClient(conn); 
+
  
-const app = express();
+await client.connect();
+const db = client.db('northwind');
+const employees = await db.collection('employees').find().toArray();
+console.log(employees);
+
+/* const app = express();
 const port = 3022;
  
 app.get('/', (req, res) => {
@@ -9,4 +21,4 @@ app.get('/', (req, res) => {
  
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
-});
+}); */
